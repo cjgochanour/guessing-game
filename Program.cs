e2d2bc6d5220113a0230a269cdfb960692e8b3ea
cjgochanour <cjgochanour@gmail.com>
2022-02-28 11:16:57 -0600

    added secret number and logic to compare guess to secret

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 ﻿using System;
 
+int SecretNumber = 42;
+
 Console.WriteLine("Guess the secret number: ");
 int UserGuess = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"You guessed {UserGuess}");
+if (UserGuess == SecretNumber)
+{
+    Console.WriteLine("You guessed correctly!");
+}
+else
+{
+    Console.WriteLine("You guessed incorrectly!");
+}
