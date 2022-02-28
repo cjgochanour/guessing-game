37b8093ce4bdc0a26a8463d147751539adf4d087
cjgochanour <cjgochanour@gmail.com>
2022-02-28 13:21:23 -0600

    user is given four tries to enter the correct number

--- Program.cs
+++ Program.cs
@@ -2,14 +2,26 @@
 
 int SecretNumber = 42;
 
-Console.WriteLine("Guess the secret number: ");
-int UserGuess = int.Parse(Console.ReadLine());
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("Guess the secret number: ");
+    int UserGuess = int.Parse(Console.ReadLine());
+    if (UserGuess != SecretNumber)
+    {
+        Console.WriteLine("You guessed incorrectly!");
+    }
+    if (UserGuess == SecretNumber)
+    {
+        Console.WriteLine("You guessed correctly!");
+        break;
+    }
+}
 
-if (UserGuess == SecretNumber)
-{
-    Console.WriteLine("You guessed correctly!");
-}
-else
-{
-    Console.WriteLine("You guessed incorrectly!");
-}
+// if (UserGuess == SecretNumber)
+// {
+//     Console.WriteLine("You guessed correctly!");
+// }
+// else
+// {
+
+// }
