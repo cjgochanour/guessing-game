a072ac70caea2571938013b9b0702198f51db720
cjgochanour <cjgochanour@gmail.com>
2022-02-28 14:38:31 -0600

    user is asked to select their difficulty

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@ Main();
 void Main()
 {
     int SecretNumber = new Random().Next(1, 101);
+    int SelectedDifficulty = 0;
     Console.WriteLine("Guess the secret number!");
+    while (SelectedDifficulty != 1 & SelectedDifficulty != 2 & SelectedDifficulty != 3)
+    {
+        Console.Write("Please select a difficulty by its number: Easy (1), Medium (2), Hard (3) > ");
+        SelectedDifficulty = int.Parse(Console.ReadLine());
+    }
     for (int i = 0; i < 4; i++)
     {
         Console.Write($"Your guess ({i + 1})> ");
@@ -25,14 +31,3 @@ void Main()
         }
     }
 }
-
-
-
-// if (UserGuess == SecretNumber)
-// {
-//     Console.WriteLine("You guessed correctly!");
-// }
-// else
-// {
-
-// }
