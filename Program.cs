dc3752de02c2a1db5502ac5da75fe236a4697bbe
cjgochanour <cjgochanour@gmail.com>
2022-02-28 14:00:10 -0600

    the secret number is now randomly generated, user is told how many guesses remain

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 
-int SecretNumber = 42;
-Console.WriteLine("Guess the secret number!");
+Main();
 
-for (int i = 0; i < 4; i++)
+void Main()
 {
-    Console.Write($"Your guess ({i + 1})> ");
-    int UserGuess = int.Parse(Console.ReadLine());
-    if (UserGuess != SecretNumber)
+    int SecretNumber = new Random().Next(1, 101);
+    Console.WriteLine("Guess the secret number!");
+    for (int i = 0; i < 4; i++)
     {
-        Console.WriteLine("You guessed incorrectly!");
-    }
-    if (UserGuess == SecretNumber)
-    {
-        Console.WriteLine("You guessed correctly!");
-        break;
+        Console.Write($"Your guess ({i + 1})> ");
+        int UserGuess = int.Parse(Console.ReadLine());
+        if (UserGuess != SecretNumber)
+        {
+            Console.WriteLine($"You guessed incorrectly! {4 - (i + 1)} guesses remain.");
+        }
+        if (UserGuess == SecretNumber)
+        {
+            Console.WriteLine("You guessed correctly!");
+            break;
+        }
     }
 }
+
+
 
 // if (UserGuess == SecretNumber)
 // {
