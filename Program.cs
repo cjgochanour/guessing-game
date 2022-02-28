43ef6b1a698f3709f76c4517d0867602aa4d160a
cjgochanour <cjgochanour@gmail.com>
2022-02-28 14:44:28 -0600

    difficulty now changes number of guesses given

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ Main();
 
 void Main()
 {
-    int SecretNumber = new Random().Next(1, 101);
     int SelectedDifficulty = 0;
     Console.WriteLine("Guess the secret number!");
     while (SelectedDifficulty != 1 & SelectedDifficulty != 2 & SelectedDifficulty != 3)
@@ -12,17 +11,34 @@ void Main()
         Console.Write("Please select a difficulty by its number: Easy (1), Medium (2), Hard (3) > ");
         SelectedDifficulty = int.Parse(Console.ReadLine());
     }
-    for (int i = 0; i < 4; i++)
+    switch (SelectedDifficulty)
+    {
+        case 1:
+            TheGame(8);
+            break;
+        case 2:
+            TheGame(6);
+            break;
+        case 3:
+            TheGame(4);
+            break;
+    }
+}
+
+void TheGame(int MaxGuesses)
+{
+    int SecretNumber = new Random().Next(1, 101);
+    for (int i = 0; i < MaxGuesses; i++)
     {
         Console.Write($"Your guess ({i + 1})> ");
         int UserGuess = int.Parse(Console.ReadLine());
         if (UserGuess > SecretNumber)
         {
-            Console.WriteLine($"Your guess was too high! {3 - i} guesses remain.");
+            Console.WriteLine($"Your guess was too high! {MaxGuesses - 1 - i} guesses remain.");
         }
         else if (UserGuess < SecretNumber)
         {
-            Console.WriteLine($"Your guess was too low! {3 - i} guesses remain.");
+            Console.WriteLine($"Your guess was too low! {MaxGuesses - 1 - i} guesses remain.");
         }
         else
         {
